b59e2995d5a77873a198ce6c823bcc56206fec28
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-03 19:26:57 -0300

    Componente com propeiedade

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 
 ReactDOM.render(
   <div> 
    <Primeiro></Primeiro>
+   <ComParametro 
+    titulo="Segundo Componente"
+    aluno="Renato "
+    nota="10.0" 
+   />
   </div>, 
   document.getElementById('root')
   );
