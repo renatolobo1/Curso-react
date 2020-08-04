2bc435af4fc44ec65709aef5f81d4b06c7fbef6d
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-04 08:46:51 -0300

    Componente card #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
-import Aleatório from './components/basicos/Aleatorio'
+import Card from './components/layout/Card';
+import Aleatório from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 
 export default () => (
-  < div id="app" >
+  <div id="app">
     <h1> Fundamentos React </h1>
-    <Aleatório min={1} max={60}/>
-    <Fragmento />
-    <ComParametro
-      titulo="Segundo Componente"
-      aluno="Renato "
-      nota="10.0"
-    />
-    <Primeiro></Primeiro>
-  </div >
-);
 
+    <Card titulo="#04 - Desafio Aleatório">
+      <Aleatório min={1} max={60} />
+      
+    </Card>
+
+    <Card titulo="#03 - Fragmento">
+      <Fragmento />
+    </Card>
+
+    <Card titulo="#02 - Com Parâmetro">
+      <ComParametro
+        titulo="Situação do Aluno"
+        aluno="Renato "
+        nota={10.0}
+      />
+    </Card>
+
+    <Card titulo="#01 - Primeiro Desafio">
+      <Primeiro></Primeiro>
+    </Card>
+  </div>
+);
