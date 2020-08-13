73e05ed6f3f455324d8af0ac0763de47c020693f
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-13 20:28:31 -0300

    Aula redenrização condicional #3

--- src/components/condicional/UsuarioInfo.jsx
+++ src/components/condicional/UsuarioInfo.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import If from './If';
+import If, {Else} from './If';
 
 export default props => {
   const usuario = props.usuario || {}
   return (
    <div>
-     <If test={usuario && usuario.nome}>
+     {/* <If test={usuario && usuario.nome}>
      Seja bem vindo <strong>{usuario.nome}</strong>!
      </If>
 
      <If test={!usuario || !usuario.nome}>
      Seja bem vindo <strong>Anonimo</strong>!
+     </If> */}
+
+     <If test={usuario && usuario.nome}>
+     Seja bem vindo <strong>{usuario.nome}</strong>!
+     <Else>
+       Seja Bem vindo <strong>Anonimo</strong>
+     </Else>
      </If>
    </div> 
   )
