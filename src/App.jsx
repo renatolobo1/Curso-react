ee9393e6d8519b88721c1a38f6325c90b3d68d54
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-14 12:05:56 -0300

    Contador #01 Class

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Card from './components/layout/Card';
 
+import Contador from './components/contador/Contador';
 import Input from './components/formulario/Input';
 import IndiretaPai from './components/cominicacao/IndiretaPai';
 import DiretaPai from './components/cominicacao/DiretaPai';
@@ -22,7 +23,11 @@ export default () => (
     <h1> Fundamentos React </h1>
     <div className="Cards">
 
-     <Card titulo="#11 - Componente Controlado" color="#8BAD39">
+      <Card titulo="#12 - Contador" color="#424242">
+        <Contador numeroInicial={10}></Contador>
+      </Card> 
+
+      <Card titulo="#11 - Componente Controlado" color="#420020">
         <Input></Input>
       </Card> 
 
---
+++ src/components/contador/Contador.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+import "./Contador.css"
+
+class Contador extends Component {
+  state = {
+    numero: this.props.numeroInicial || 0,
+    passo: this.props.passoInicial || 5,
+  };
+
+  inc=()=> {
+    this.setState({
+      numero: this.state.numero + this.state.passo,
+    });
+  };
+
+  dec=()=> {
+    this.setState({
+      numero: this.state.numero - this.state.passo,
+    });
+  };
+
+  setPasso = (e) => {
+    this.setState({
+      passo: +e.target.value,
+    });
+  }
+
+  render(){
+    return (
+      <div className="Contador">
+        <h2>Contador</h2>
+        <h3>{this.state.numero}</h3>
+        <div>
+          <label htmlFor="passoInput">Passo:</label>
+          <input
+            id="passoInput"
+            type="number"
+            value={this.state.passo}
+            onChange={this.setPasso}
+          />
+        </div>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    );
+  }
+
+
+}
+
+export default Contador;
