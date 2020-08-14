bab5fa8618ceb837a2490a5940a631c0e03c37f5
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-14 15:22:41 -0300

    Contador #02 Class

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
+import Dysplay from './Dysplay';
+import Botoes from './Botoes';
+import PassoForm from './PassoForm';
+
 import "./Contador.css"
+
 
 class Contador extends Component {
   state = {
@@ -20,9 +25,9 @@ class Contador extends Component {
     });
   };
 
-  setPasso = (e) => {
+  setPasso = (novoPasso) => {
     this.setState({
-      passo: +e.target.value,
+      passo: novoPasso,
     });
   }
 
@@ -30,18 +35,10 @@ class Contador extends Component {
     return (
       <div className="Contador">
         <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-          <label htmlFor="passoInput">Passo:</label>
-          <input
-            id="passoInput"
-            type="number"
-            value={this.state.passo}
-            onChange={this.setPasso}
-          />
-        </div>
-        <button onClick={this.inc}>+</button>
-        <button onClick={this.dec}>-</button>
+        <Dysplay numero={this.state.numero}/>
+        <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+        <Botoes setInc={this.inc} setDec={this.dec}/>
+        
       </div>
     );
   }
