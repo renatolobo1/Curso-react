43894cd98eddeddea73b185726f387f83403c3bf
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-14 08:49:54 -0300

    Aula Componente com Estado

--- src/components/cominicacao/IndiretaFilho.jsx
+++ src/components/cominicacao/IndiretaFilho.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 export default props => {
-  
+  const min = 10
+  const max = 60
+  const gerarIdade = () => parseInt(Math.random()*(max-min)) + min
+  const gerarNerd = () => Math.random() >0.5
   return(
     <div>
       <div>Filho</div>
       <button onClick={
         function (e) {
-          props.quandoClicar('joão', 53, true)
+          props.quandoClicar('joão', gerarIdade() , gerarNerd())
         }
       }>
         Fornecer Informações
--- src/components/cominicacao/IndiretaPai.jsx
+++ src/components/cominicacao/IndiretaPai.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 export default props => {
-  let nome = '?'
-  let idade = 0
-  let nerd = false
+  const [nome, setNome] = useState('Pedro')
+  const [idade, setIdade] = useState(10)
+  const [nerd, setNerd] = useState(true)
 
-  function fornecerInformacoes(nomeParam, idadeParam, nerdParam){
-   nome = nomeParam
-   idade = idadeParam
-   nerd= nerdParam
+  function fornecerInformacoes(nome, idade, nerd){
+    setNome(nome) 
+    setIdade(idade) 
+    setNerd(nerd)
 }
 
   return(
